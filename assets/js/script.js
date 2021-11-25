fddcc607c4f6350fa82f34df710fb201e8e88ed5
lsimpson <lsimpson@teamsi.com>
2021-11-24 19:25:47 -0600

    few changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var formHandlerEl = function(event) {
+var global = {};
+var formHandlerEl = function (event) {
     event.preventDefault();
     var cityStateInputEl = document.getElementById("location-input").value;
     // run api call next
 
     var parksApiUrl = "https://developer.nps.gov/api/v1/parks?limit=50&start=0&q=" + cityStateInputEl + "&api_key=xG0N7G0NIR00rad6uzGstrePJgkPJ12OyeOMTr9q";
     var foodApiUrl = "https://api.documenu.com/v2/restaurants/search/fields?state=" + cityStateInputEl + "&key=0d461c352166be6cd4a1a1e0925996b4";
-    
+
     //grabbing users search input
     var searchLocation = document.getElementById("location-input").value;
     console.log(searchLocation);
@@ -17,15 +18,13 @@ var formHandlerEl = function(event) {
     console.log(diningCheck);
     console.log(lodgingCheck);
 
-    fetch(parksApiUrl).then(function(response) {
+    fetch(parksApiUrl).then(function (response) {
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function (data) {
                 console.log(data.data);
-                for (i = 0; i < 4; i++) {
-                    var lon = data.data[i].longitude;
-                    var lat = data.data[i].latitude;
+                for (i = 0; i < 3; i++) {
                     var name = data.data[i].name;
-                    var zipCode = data.data[i].addresses[0].postalCode;
+                    global.zipCode = data.data[i].addresses[0].postalCode;
                     var descrip = data.data[i].description;
 
                     var parkNames = document.getElementById("park-list");
@@ -33,32 +32,41 @@ var formHandlerEl = function(event) {
                     parkNameList.textContent = name;
                     parkNames.appendChild(parkNameList);
 
-                   var parkLinks = document.createElement("p");
-                   parkLinks.innerHTML = "<a href=https://api.documenu.com/v2/restaurants/" + zipCode + "&key=0d461c352166be6cd4a1a1e0925996b4> Link here </a>";
-                   parkNames.appendChild(parkLinks);
+                    var parkLinks = document.createElement("p");
+                    parkLinks.innerHTML = "<button>Click me</button>";
+                    parkNames.appendChild(parkLinks);
+                    parkLinks.setAttribute("id", global.zipCode);
+                    document.getElementById("park-description").innerText = descrip;
 
                 }
             })
         }
     });
 
-    fetch(foodApiUrl).then(function(response) {
-        if(response.ok) {
-            response.json().then(function(data) {
-                console.log(data);
-            })
-        }
-
-    });
 
     // when data comes in save in localStorage
 
     // display results in appropriate list
-
 }
+var foodHandler = function (event) {
+    event.preventDefault();
+    var foodApiUrl = "https://api.documenu.com/v2/restaurants/75052&key=0d461c352166be6cd4a1a1e0925996b4";
+    fetch(foodApiUrl).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+
+                })
+            }
+        }
+
+
+    )
+};
 
 // if (document.getElementById("park-checkmark").checked = true) {
 //     console.log(cityStateInputEl)
 // }
 
 document.getElementById("location").addEventListener("submit", formHandlerEl);
+document.getElementById(global.zipCode).addEventListener("onclick", foodHandler);
